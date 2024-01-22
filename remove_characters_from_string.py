59d4b41790ebbc9067c31bd5057f71cf3a75f665
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 18:29:17 +0800

    print tive and native

--- remove_characters_from_string.py
+++ remove_characters_from_string.py
@@ -1,2 +1,10 @@
 #Exercise 4: Remove first n characters from a string
 #Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+def remove_chars(word, n):
+  
+#output should be tive
+    print(remove_chars("pynative", 4)) 
+
+#output should be native
+    print(remove_chars("pynative", 2)) 
