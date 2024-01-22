a250d158a39358921cb1cdb9147d786d3a936720
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-22 19:05:08 +0800

    finalized

--- remove_characters_from_string.py
+++ remove_characters_from_string.py
@@ -2,10 +2,14 @@
 #Write a program to remove characters from a string starting from zero up to n and return a new string.
 
 def remove_chars(word, n):
-     
+    print('Original string:', word)
+    if n < len(word):
+        result = word[n:]
+        return result
+    
+print("Remove characters from the string")
 
 #Print: output should be tive
-    print("Remove characters from the string")
 print(remove_chars("pynative", 4)) 
 
 #Print: output should be native
